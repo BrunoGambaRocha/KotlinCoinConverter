d1c084157dec76b1bf0527de0f2e24c58ca7a283
Bruno Rocha <bruno.roch@gmail.com>
2021-10-16 13:24:26 -0300

    adding retrofit configuration - DataModules and AwesomeService

--- app/src/main/java/br/com/brunoti/kotlincoinconverter/App.kt
+++ app/src/main/java/br/com/brunoti/kotlincoinconverter/App.kt
@@ -1,5 +1,19 @@
 package br.com.brunoti.kotlincoinconverter
 
 import android.app.Application
+import br.com.brunoti.kotlincoinconverter.data.di.DataModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class App : Application()
+class App : Application() {
+
+	override fun onCreate() {
+		super.onCreate()
+
+		startKoin {
+			androidContext(this@App)
+		}
+
+		DataModules.load()
+	}
+}
