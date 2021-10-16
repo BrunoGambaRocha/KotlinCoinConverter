40c9d07102feffecde86c6ab4ddfa2eab7ee69a3
Bruno Rocha <bruno.roch@gmail.com>
2021-10-16 11:49:34 -0300

    add eventings in lists coins MainActivity

--- app/src/main/java/br/com/brunoti/kotlincoinconverter/ui/MainActivity.kt
+++ app/src/main/java/br/com/brunoti/kotlincoinconverter/ui/MainActivity.kt
@@ -1,15 +1,43 @@
 package br.com.brunoti.kotlincoinconverter.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import br.com.brunoti.kotlincoinconverter.core.extensions.text
+import br.com.brunoti.kotlincoinconverter.data.model.Coin
 import br.com.brunoti.kotlincoinconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+		bindAdapters()
+		bindListeners()
+	}
+
+	private fun bindAdapters() {
+		val list = Coin.values()
+		val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+		binding.tvFrom.setAdapter(adapter)
+		binding.tvTo.setAdapter(adapter)
+
+		binding.tvFrom.setText(Coin.BRL.name, false)
+		binding.tvTo.setText(Coin.USD.name, false)
+	}
+
+	private fun bindListeners() {
+		binding.tilValue.editText?.doAfterTextChanged {
+			binding.btnConverter.isEnabled = it != null && it.toString().isNotEmpty()
+		}
+
+		binding.btnConverter.setOnClickListener {
+			Log.e("TAG", "bindListeners: " + binding.tilValue.text)
+		}
+	}
 }
